b3d649bc7357b5fde6d6bb0ff3a2089b1def3f70
CarsonRoscoe <CarsonRoscoe7@gmail.com>
2021-10-22 15:08:31 -0700

    Extended test suote to V1 scenario

---
+++ Ethereum/test/V1/GoGBattlesToken_V1.js
@@ -0,0 +1,36 @@
+const { Contract, ContractFactory } = require('@ethersproject/contracts');
+const { SignerWithAddress } = require('@nomiclabs/hardhat-ethers/signers');
+const { arrayify } = require('@ethersproject/bytes');
+const { expect } = require('chai');
+const { Wallet } = require('ethers');
+const { ethers } = require('hardhat');
+
+const Scenario_V1 = require('./V1.js');
+
+let fullNames = {};
+let dappFactory = {};
+let dapp = {};
+let user = {}
+
+async function resetScenario() {
+  await Scenario_V1.resetScenarioAsync();
+  let scenario = Scenario_V1.getScenario();
+  fullNames = scenario.fullNames;
+  dappFactory = scenario.dappFactory;
+  dapp = scenario.dapp;
+  user = scenario.user;
+}
+
+describe('GoG: Battles\' Token Test Suite', () => {
+  it('Scenario_V1 Loaded', async () => { await resetScenario(); });
+
+  it("GoGBattlesToken\'s instantiation was a success. It should have the \'Symbol\' GOGB, name \'\', and initial supply of 0.", async () => {
+    await expect(await dapp.GoGBattlesToken.totalSupply()).to.equal(0);
+    await expect(await dapp.GoGBattlesToken.name()).to.equal('GoG: Battles\' Token');
+    await expect(await dapp.GoGBattlesToken.symbol()).to.equal('GOGB');
+  });
+  it("Deployer is setup with the DEFAULT_ADMIN_ROLE for access control.", async () => {
+  });
+  it("Only the coordinator can call mint, and mint is fully implemented, including batch mints.", async () => {
+  });
+});
