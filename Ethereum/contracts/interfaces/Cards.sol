db083c4e5d804454a76e834a3b6e0766d18cea1c
CarsonRoscoe <CarsonRoscoe7@gmail.com>
2021-11-05 11:14:38 -0400

    Card tests

--- Ethereum/contracts/interfaces/Cards.sol
+++ Ethereum/contracts/interfaces/Cards.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 interface Cards is IERC1155 {
-    function setGoGBattlesTokenAndCardFactory(address gogBattlesToken, address gogBattlesCardFactory) external;
+    function setGoGBattlesTokenAndCardFactory(address gogBattlesToken, address gogBattlesCardFactory, address gogBattlesVault) external;
     function setURI(string memory newuri) external;
-    function mint(uint256 value, bytes memory data) external returns(uint256);
+    function mintUnbacked(address to, uint256 cardId) external returns(uint256);
     function mintBatch(address to, uint256[] memory cardIds, uint256[] memory values) external returns(uint256[] memory);
     function mintPack(address to, uint256 tokenAmount) external returns(uint256[] memory, uint256);
     function burnBatch(address owner, uint256[] memory ids, uint256[] memory amounts) external;
--- Ethereum/contracts/standard/GoGBattlesCards_V1.sol
+++ Ethereum/contracts/standard/GoGBattlesCards_V1.sol
@@ -7,11 +7,12 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/CardFactory.sol";
 import "../interfaces/Cards.sol";
+import "../interfaces/Vault.sol";
 
-
-struct Card {
-    uint backedValue;
-}
+/**
+    TODO:
+    - Lock duration setable by DEFAULT_ADMIN
+ */
 
 contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
@@ -20,11 +21,13 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
     
     IERC20 _token;
     CardFactory _factory;
+    Vault _vault;
     
+    mapping(uint => uint) _tokenIdToCardId;
     mapping(uint => uint) _valueOfCards;
     mapping(uint => uint) _timelockOnCards;
     mapping(address => uint) _valueOfAccount;
-    uint _nextTokenID;
+    uint public nextTokenID;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() ERC1155("https://guildsofgods.com/cards/") {
@@ -34,28 +37,33 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
         _setupRole(COORDINATOR_ROLE, msg.sender);
     }
 
-    function setGoGBattlesTokenAndCardFactory(address gogBattlesToken, address gogBattlesCardFactory) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setGoGBattlesTokenAndCardFactory(address gogBattlesToken, address gogBattlesCardFactory, address gogBattlesVault) public override onlyRole(DEFAULT_ADMIN_ROLE) {
         _token = IERC20(gogBattlesToken);
         _factory = CardFactory(gogBattlesCardFactory);
+        _vault = Vault(gogBattlesVault);
+        require(address(_token) != address(0)); 
+        require(address(_factory) != address(0)); 
+        require(address(_vault) != address(0)); 
     }
 
     function setURI(string memory newuri) public override onlyRole(URI_SETTER_ROLE) {
         _setURI(newuri);
     }
 
-    function mint(uint256 value, bytes memory data) public override onlyRole(MINTER_ROLE) returns(uint256) {
-        uint id = _nextTokenID++;
-        _valueOfCards[id] = value;
-        _mint(msg.sender, id, 1, data);
+    function mintUnbacked(address to, uint cardId) public override onlyRole(MINTER_ROLE) returns(uint256) {
+        uint id = nextTokenID++;
+        _mint(to, id, 1, "0x0");
+        _tokenIdToCardId[id] = cardId;
         return id;
     }
 
-    function mintBatch(address to, uint256[] memory cardIds, uint256[] memory values) public override onlyRole(MINTER_ROLE) returns(uint256[] memory) {
+    function mintBatch(address to, uint256[] memory cardIds, uint256[] memory values) public override onlyRole(MINTER_ROLE) returns(uint256[] memory tokenIds) {
         require(cardIds.length == values.length, "Arrays must be of the same size");
         uint256[] memory ids = new uint256[](cardIds.length);
         uint256[] memory amounts = new uint256[](cardIds.length);
-        for(uint i = 0; i < amounts.length; ++i) {
-            ids[i] = _nextTokenID++;
+        for(uint i = 0; i < cardIds.length; ++i) {
+            ids[i] = nextTokenID++;
+            _tokenIdToCardId[ids[i]] = cardIds[i];
             _valueOfCards[ids[i]] = values[i];
             amounts[i] = 1;
         }
@@ -63,18 +71,20 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
         return ids;
     }
     
-    function mintPack(address to, uint256 tokenAmount) public override onlyRole(MINTER_ROLE) returns(uint256[] memory, uint256){
-        require(_token.transferFrom(msg.sender, address(this), tokenAmount), "Transfer must succeed");
+    function mintPack(address to, uint256 tokenAmount) public override onlyRole(MINTER_ROLE) returns(uint256[] memory tokenIds, uint256 tokensRemaining){
+        require(address(_token) != address(0), "Setup incomplete. Token address must be set.");
+        require(address(_vault) != address(0), "Setup incomplete. Vault address must be set.");
+        require(address(_factory) != address(0), "Setup incomplete. Card Factory address must be set.");
+        require(tokenAmount > 0, "Minting a pack requires a deposit.");
         (uint[] memory cardIds, uint[] memory cardValues, uint tokensRemaining) = _factory.rollCards(tokenAmount);
+        require(tokenAmount - tokensRemaining > 0, "Cannot mint a pack if given a full refund.");
+        require(_token.transferFrom(msg.sender, address(_vault), tokenAmount - tokensRemaining), "Token transfer must succeed for the tokenAmount provided, minus remaining tokens.");
         uint256[] memory tokenIds = mintBatch(to, cardIds, cardValues);
-        if (tokensRemaining > 0) {
-            _token.approve(msg.sender, tokensRemaining);
-        }
         return (tokenIds, tokensRemaining);
     }
 
     function burnBatch(address owner, uint256[] memory ids, uint256[] memory amounts) public override(Cards, ERC1155Burnable) onlyRole(COORDINATOR_ROLE) {
-        super.burnBatch(owner, ids, amounts);
+        super._burnBatch(owner, ids, amounts);
     }
     
     function burnBatch(address owner, uint256[] memory ids) public override onlyRole(COORDINATOR_ROLE) {
@@ -82,13 +92,7 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
         for(uint i = 0; i < ids.length; ++i) {
             amounts[i] = 1;
         }
-        super.burnBatch(owner, ids, amounts);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function supportsInterface(bytes4 interfaceId) public view override(IERC165, ERC1155, AccessControl) returns (bool) {
-        return super.supportsInterface(interfaceId);
+        burnBatch(owner, ids, amounts);
     }
     
     function backingValueOf(uint[] memory cardIds) public override view returns(uint) {
@@ -102,12 +106,17 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
     function backingBalanceOf(address user) public override view returns(uint) {
         return _valueOfAccount[user];
     }
+
+    function getCardId(uint tokenId) public view returns(uint) {
+        require(tokenId < nextTokenID, "Token does not exist yet.");
+        return _tokenIdToCardId[tokenId];
+    }
     
     function _beforeTokenTransfer( address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data ) internal override virtual {
         // mint case
         if (from == address(0)) {
             for(uint i = 0; i < ids.length; ++i) {
-                _timelockOnCards[ids[i]] = block.timestamp;
+                _timelockOnCards[ids[i]] = block.timestamp; // + howeverlong the lock period is
             }
         }
         
@@ -129,5 +138,10 @@ contract GoGBattlesCards_V1 is Cards, ERC1155, AccessControl, ERC1155Burnable {
         if (to != address(0)) {
             _valueOfAccount[to] += valueOfCards;
         }
+    }
+
+    // The following functions are overrides required by Solidity.
+    function supportsInterface(bytes4 interfaceId) public view override(IERC165, ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
